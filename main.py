ebff56101cc1be3dfd24ca855c7c7e6057728b50
szychowskim <szychowskim@educ.college-montreal.qc.ca>
2023-08-31 15:41:41 -0400

    Terminer

---
+++ main.py
@@ -0,0 +1,6 @@
+#créé par Mikolai Szychowski
+#créé le 31 aout 2023
+#TP1
+print('Bonjour! Pret à etre épaté?')     #introduction
+print(len(input('Écrivez ici ce que vous désirez:').split()))  #Définition variable
+print('est le nombre de mots que contient votre phrases.')     #Conclusion
